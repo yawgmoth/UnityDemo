897b75472bc7b159c8ab17231078248bf7e218aa
Markus Eger <markus.eger@gmail.com>
2019-01-16 22:13:51 -0600

    initial commit

---
+++ Assets/Scripts/ControlHovercraft.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlHovercraft : MonoBehaviour
+{
+
+    public Waypoint target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sgn = Mathf.Sign(- Input.GetAxis("Vertical"));
+        if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.001) sgn = 0;
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 100*sgn, 0));
+
+        sgn = Mathf.Sign(Input.GetAxis("Horizontal"));
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) < 0.001) sgn = 0;
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(100 * sgn, 0, 0));
+
+        float dir = 0;
+        dir = Mathf.Clamp((Input.mousePosition.x - Screen.width / 2)/100, -3, 3);
+        transform.rotation = Quaternion.Euler(0, dir, 0) * transform.rotation;
+        
+
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    public Waypoint next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ControlHovercraft hover = other.gameObject.GetComponent<ControlHovercraft>();
+        if (hover != null)
+        {
+            if (hover.target == this)
+            {
+                hover.target = next;
+                next.GetComponent<MeshRenderer>().material.color = Color.red;
+                GetComponent<MeshRenderer>().material.color = Color.white;
+                Debug.Log("next target!");
+            }
+        }
+    }
+}
